ede43322259606e769d7ad27a68a75f3ae445dad
Olivier Rabot <olivierrabot@oli-mbp.local>
2023-05-10 12:53:52 +0200

    [FormAlert] display link on success

--- components/FormAlert.tsx
+++ components/FormAlert.tsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
-import cn from 'classnames';
+import { useEffect, useState } from "react";
+import cn from "classnames";
+import Link from "next/link"
 
 interface FormAlertProps {
-	type: 'success' | 'warning';
+	type: "success" | "warning";
+	originalUrl: string;
+	shortUrl: string;
 }
 
-const FormAlert: React.FC<FormAlertProps> = ({ type }) => {
+const FormAlert: React.FC<FormAlertProps> = ({ type, originalUrl = "", shortUrl = "" }) => {
 	const [show, setShow] = useState(false);
 
 	useEffect(() => {
@@ -15,14 +18,16 @@ const FormAlert: React.FC<FormAlertProps> = ({ type }) => {
 	}, [type]);
 
 	const alertClass = cn({
-		'bg-validate500 text-validate400': type === 'success',
-		'bg-alert400 text-alert500': type === 'warning',
+		"bg-validate500 text-validate400": type === "success",
+		"bg-alert400 text-alert500": type === "warning",
 	});
 
+	const RedirectLink = <Link href={originalUrl}>{shortUrl}</Link>;
+
 	return (
-		<div className={cn('text-left w-full px-5 py-3 mx-auto rounded-md', alertClass)}>
-			{type === 'success' && <p>Voici le lien raccourci : https//localhost:3000/4hxNeK</p>}
-			{type === 'warning' && <p>L’url saisie est invalide.</p>}
+		<div className={cn("text-left w-full px-5 py-3 mx-auto rounded-md", alertClass)}>
+			{type === "success" && <p>Voici le lien raccourci : {RedirectLink}</p>}
+			{type === "warning" && <p>L’url saisie est invalide.</p>}
 		</div>
 	);
 };
