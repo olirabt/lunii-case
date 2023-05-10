86939004bbfaf5ba60218b790d4697d34bb58092
Olivier Rabot <olivierrabot@oli-mbp.local>
2023-05-10 22:01:34 +0200

    update tests: create snapshot for [FormAlert]

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,7 +1,14 @@
 import { render } from '@testing-library/react'
-import Home from '@/pages/index'
+import FormAlert from "@/components/FormAlert";
 
-it('renders homepage unchanged', () => {
-  const { container } = render(<Home />)
-  expect(container).toMatchSnapshot()
-})
+describe("FormAlert", () => {
+  it("renders the success message correctly", () => {
+    const { container } = render(<FormAlert type="success" originalUrl="http://google.com/somelongparameter" shortUrl="https://shorturl.com" error={null} />)
+    expect(container).toMatchSnapshot()
+  });
+
+  it("renders the warning message correctly", () => {
+    const { container } = render(<FormAlert type="warning" originalUrl="google" shortUrl="" error="L’url saisie est invalide" />)
+    expect(container).toMatchSnapshot()
+  });
+});
