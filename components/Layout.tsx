972ddc27c4f8b1e708fa1b0e488b670c53073b9e
Olivier Rabot <olivierrabot@oli-mbp.local>
2023-05-10 15:47:40 +0200

    fix fonts loading

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,13 +10,9 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap"
-          rel="stylesheet"
-        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-			<Header/>
+      <Header />
       <main>{children}</main>
     </>
   );
